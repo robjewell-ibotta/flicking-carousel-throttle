75cda2e16f98cb8bcbacdfdef4568f6c07aa289c
Rob Jewell <rob.jewell@ibotta.com>
2021-07-01 12:02:20 -0400

    WIP: trying to setup project to run locally

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Flicking 
+      :options="{ circular: true, renderOnlyVisible: true }"
+      >
+      
+      <img :src="banner.image.url" :alt="banner.image.alt" class="banner-image" />
+    </Flicking>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { Flicking } from '@egjs/vue-flicking';
+import mockResponse from '@/components/mockBanners';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      banners: mockResponse
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Flicking
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "@egjs/flicking/dist/flicking.css"; // Supports IE10+, using CSS flex
+import "@egjs/flicking/dist/flicking.inline.css"; //Supports IE9+, using CSS inline-box
+
 Vue.config.productionTip = false
 
 new Vue({
