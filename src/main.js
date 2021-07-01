e2a4ce09314113ffc1bf612ec708f3b8e99e9d90
Rob Jewell <rob.jewell@ibotta.com>
2021-07-01 12:21:31 -0400

    WIP: still trying to setup the project locally

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import "@egjs/flicking/dist/flicking.css"; // Supports IE10+, using CSS flex
-import "@egjs/flicking/dist/flicking.inline.css"; //Supports IE9+, using CSS inline-box
+import "@egjs/flicking/dist/flicking-inline.css"; //Supports IE9+, using CSS inline-box
 
 Vue.config.productionTip = false
 
