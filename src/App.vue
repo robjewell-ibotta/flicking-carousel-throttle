ed59f78fade16bd40faa61d2c42a1e4975ab349a
Rob Jewell <rob.jewell@ibotta.com>
2021-07-08 09:50:02 -0400

    Working example

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,23 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Flicking :options="{ circular: true }">
-      <img 
-        v-for="banner in banners.data.images" 
-        :key="banner.id" 
-        :src="banner.image.url" 
-        :alt="banner.image.alt" 
-        class="banner-image" />
-    </Flicking>
+    <div class='carousel-container'>
+      <Flicking v-if="banners.data.images.length" :options="{ circular: true, renderOnlyVisible: true }">
+        <img 
+          v-for="banner in banners.data.images" 
+          :key="banner.id" 
+          :src="banner.image.url" 
+          :alt="banner.image.alt" 
+          class="banner-image" />
+      </Flicking>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import mockResponse from '@/components/mockBanners';
+import { Flicking } from '@egjs/vue-flicking';
 
 
 export default {
@@ -26,7 +29,8 @@ export default {
     }
   },
   components: {
-    HelloWorld
+    HelloWorld,
+    Flicking
   }
 }
 </script>
@@ -39,5 +43,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.carousel-container {
+
+  margin-bottom: 10px;
+}
+
+.carousel-container img {
+  width: 100%;
+  border-radius: 9.5px;
+  -webkit-user-drag: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,6 @@ import App from './App.vue'
 import "@egjs/flicking/dist/flicking.css"; // Supports IE10+, using CSS flex
 import "@egjs/flicking/dist/flicking-inline.css"; //Supports IE9+, using CSS inline-box
 
-import VueFlicking from "@egjs/vue-flicking";
-
-
-Vue.use(VueFlicking);
-
 Vue.config.productionTip = false
 
 new Vue({
